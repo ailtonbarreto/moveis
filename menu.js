19c305ecbb2a8be0aff3cf4ab24bbc93b4847382
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2026-09-08 21:43:36 -0300

    update geral

--- menu.js
+++ menu.js
@@ -7,27 +7,10 @@ const closeBtn = document.getElementById("close_menu");
 
 openBtn.addEventListener("click", () => {
     menu.classList.add("open");
-
-
 });
 
-openBtn.addEventListener("click", () => {
+closeBtn.addEventListener("click", () => {
     menu.classList.remove("open");
-
 });
 
-
-// document.querySelectorAll(".has-submenu").forEach(item => {
-//     item.addEventListener("click", () => {
-
-
-//         document.querySelectorAll(".has-submenu.open").forEach(openItem => {
-//             if (openItem !== item) {
-//                 openItem.classList.remove("open");
-//             }
-//         });
-
-//         item.classList.toggle("open");
-//     });
-// });
 
